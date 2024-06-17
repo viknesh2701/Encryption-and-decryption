5e8150af7afd478bededdc509a0460bff5036618
VIKNESH K <128950710+viknesh2701@users.noreply.github.com>
2024-06-17 15:47:23 +0530

    Update Encryption.py

--- Encryption.py
+++ Encryption.py
@@ -1,100 +1,105 @@
 from Crypto import Random
-fromCrypto.Cipher import AES
-importos
-importos.path
-fromos import listdir
-fromos.path import isfile, join
+from Crypto.Cipher import AES
+import os
 import time
-classEncryptor:
-def __init__(self, key):
-self.key = key
-def pad(self, s):
-return s + b"\0" * (AES.block_size - len(s) % AES.block_size)
-def encrypt(self, message, key, key_size=256):
-message = self.pad(message)
-iv = Random.new().read(AES.block_size)
-cipher = AES.new(key, AES.MODE_CBC, iv)
-return iv + cipher.encrypt(message)
-defencrypt_file(self, file_name):
-with open(file_name, 'rb') as fo:
-plaintext = fo.read()
-enc = self.encrypt(plaintext, self.key)
-with open(file_name + ".enc", 'wb') as fo:
-fo.write(enc)
-os.remove(file_name)
-def decrypt(self, ciphertext, key):
-iv = ciphertext[:AES.block_size]
-cipher = AES.new(key, AES.MODE_CBC, iv)
-plaintext = cipher.decrypt(ciphertext[AES.block_size:])
-returnplaintext.rstrip(b"\0")
-defdecrypt_file(self, file_name):
-with open(file_name, 'rb') as fo:
-ciphertext = fo.read()
-dec = self.decrypt(ciphertext, self.key)
-with open(file_name[:-4], 'wb') as fo:
-fo.write(dec)
-os.remove(file_name)
-defgetAllFiles(self):
-dir_path = os.path.dirname(os.path.realpath(__file__))
-dirs = []
-fordirName, subdirList, fileList in os.walk(dir_path):
-forfname in fileList:
-if (fname != 'script.py' and fname != 'data.txt.enc'):
-dirs.append(dirName + "\\" + fname)
-returndirs
-defencrypt_all_files(self):
-dirs = self.getAllFiles()
-forfile_name in dirs:
-self.encrypt_file(file_name)
-defdecrypt_all_files(self):
-dirs = self.getAllFiles()
-forfile_name in dirs:
-self.decrypt_file(file_name)
-key=
-b'[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\
-x94\x9d(\x9e'
+
+class Encryptor:
+    def __init__(self, key):
+        self.key = key
+
+    def pad(self, s):
+        return s + b"\0" * (AES.block_size - len(s) % AES.block_size)
+
+    def encrypt(self, message, key, key_size=256):
+        message = self.pad(message)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        return iv + cipher.encrypt(message)
+
+    def encrypt_file(self, file_name):
+        with open(file_name, 'rb') as fo:
+            plaintext = fo.read()
+        enc = self.encrypt(plaintext, self.key)
+        with open(file_name + ".enc", 'wb') as fo:
+            fo.write(enc)
+        os.remove(file_name)
+
+    def decrypt(self, ciphertext, key):
+        iv = ciphertext[:AES.block_size]
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        plaintext = cipher.decrypt(ciphertext[AES.block_size:])
+        return plaintext.rstrip(b"\0")
+
+    def decrypt_file(self, file_name):
+        with open(file_name, 'rb') as fo:
+            ciphertext = fo.read()
+        dec = self.decrypt(ciphertext, self.key)
+        with open(file_name[:-4], 'wb') as fo:
+            fo.write(dec)
+        os.remove(file_name)
+
+    def get_all_files(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        dirs = []
+        for dirName, subdirList, fileList in os.walk(dir_path):
+            for fname in fileList:
+                if fname != 'script.py' and fname != 'data.txt.enc':
+                    dirs.append(os.path.join(dirName, fname))
+        return dirs
+
+    def encrypt_all_files(self):
+        dirs = self.get_all_files()
+        for file_name in dirs:
+            self.encrypt_file(file_name)
+
+    def decrypt_all_files(self):
+        dirs = self.get_all_files()
+        for file_name in dirs:
+            self.decrypt_file(file_name)
+
+
+key = b'[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e'
 enc = Encryptor(key)
 clear = lambda: os.system('cls')
-ifos.path.isfile('data.txt.enc'):
-while True:
-password = str(input("Enter password: "))
-enc.decrypt_file("data.txt.enc")
-p = '' “
-with open("data.txt", "r") as f:
-p = f.readlines()
-if p[0] == password:
-enc.encrypt_file("data.txt")
-break
-while True:
-clear()
-choice = int(input("1. Press '1' to encrypt file.\n2. Press '2' to decrypt file.\n3. Press '3' to Encrypt all
-files in the directory.\n4. Press '4' to decrypt all files in the directory.\n5. Press '5' to
-exit.\n"))
-clear()
-if choice == 1:
-enc.encrypt_file(str(input("Enter name of file to encrypt: ")))
-elif choice == 2:
-enc.decrypt_file(str(input("Enter name of file to decrypt: ")))
-elif choice == 3:
-enc.encrypt_all_files()
-elif choice == 4:
-enc.decrypt_all_files()
-elif choice == 5:
-exit()
+
+if os.path.isfile('data.txt.enc'):
+    while True:
+        password = str(input("Enter password: "))
+        enc.decrypt_file("data.txt.enc")
+        with open("data.txt", "r") as f:
+            p = f.readlines()
+        if p[0] == password:
+            enc.encrypt_file("data.txt")
+            break
+
+    while True:
+        clear()
+        choice = int(input("1. Press '1' to encrypt file.\n2. Press '2' to decrypt file.\n3. Press '3' to encrypt all files in the directory.\n4. Press '4' to decrypt all files in the directory.\n5. Press '5' to exit.\n"))
+        clear()
+        if choice == 1:
+            enc.encrypt_file(str(input("Enter name of file to encrypt: ")))
+        elif choice == 2:
+            enc.decrypt_file(str(input("Enter name of file to decrypt: ")))
+        elif choice == 3:
+            enc.encrypt_all_files()
+        elif choice == 4:
+            enc.decrypt_all_files()
+        elif choice == 5:
+            exit()
+        else:
+            print("Please select a valid option!")
 else:
-print("Please select a valid option!")
-else:
-while True:
-clear()
-password = str(input("Setting up stuff. Enter a password that will be used for
-decryption: "))
-repassword = str(input("Confirm password: "))
-if password == repassword:break
-else:
-print("Passwords Mismatched!")
- f = open("data.txt", "w+")
-f.write(password)
-f.close()
-enc.encrypt_file("data.txt")
-print("Please restart the program to complete the setup")
-time.sleep(15)
+    while True:
+        clear()
+        password = str(input("Setting up stuff. Enter a password that will be used for decryption: "))
+        repassword = str(input("Confirm password: "))
+        if password == repassword:
+            break
+        else:
+            print("Passwords Mismatched!")
+    
+    with open("data.txt", "w+") as f:
+        f.write(password)
+    enc.encrypt_file("data.txt")
+    print("Please restart the program to complete the setup")
+    time.sleep(15)
